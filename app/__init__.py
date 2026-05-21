77e85dbfd45e1ba50020bd92486aa6f9fdfffd21
lawintun <lawintun1000@gmail.com>
2026-05-21 12:46:15 +0630

    blabla

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+
+    # Register blueprints
+    from app.main import main_bp
+    app.register_blueprint(main_bp)
+
+    return app  
---
+++ app/main/routes.py
@@ -0,0 +1,8 @@
+from flask import render_template
+
+from app.main import main_bp
+
+
+@main_bp.route('/')
+def home():
+    return render_template('guest.html')
